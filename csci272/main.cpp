a95a5cb1ea432a9be4c3ae8fd37613b6ee454c32
Satyam Mittal <satyam101905@gmail.com>
2022-08-28 12:00:35 -0700

    Initial Commit

---
+++ csci272/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  csci272
+//
+//  Created by Satyam Mittal on 28/08/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
